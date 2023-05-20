acf9f9760242c9d59c015b5b696cc5b4ee5fa8b8
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-05-21 03:10:54 +0530

    update base-route.ts

--- src/routes/base-route.ts
+++ src/routes/base-route.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
 import { ExpressResponse } from '../response';
+import { tsFunction } from '../config/config';
 
 const app = Router()
 
 app.get('/v2/set', async (req: Request, res: Response) => {
-    return ExpressResponse(res, true, 200, 'Success')
+    return ExpressResponse(res, true, 200, tsFunction())
 })
 
 export default app
