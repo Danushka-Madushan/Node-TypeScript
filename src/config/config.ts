2e1a6b7f5561cbbc2dd81b267fff27d8027dbb02
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-02 23:46:39 +0530

    #1 Interface and extends pratice

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,3 +3,28 @@ import { config } from 'dotenv';config();
 export const tsFunction = (): string => {
     return 'Hello from TypeScript'
 }
+
+interface TData {
+    name: string,
+    address: Array<{
+        country?: "LK" | "USA"
+        key: string,
+        value: string | boolean
+    }>
+}
+
+interface TPerson extends TData {
+    age: number
+}
+
+export const human: TPerson = {
+    name: "Alex Murphy",
+    address: [
+        {
+            key: "District",
+            value: "Hambantota",
+            country: "LK"
+        }
+    ],
+    age: 12
+}
