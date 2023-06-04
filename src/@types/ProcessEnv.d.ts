ccb082f625680f1039d929e5aea4a731459fb847
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-04 15:06:42 +0530

    Process Environment Types

---
+++ src/@types/ProcessEnv.d.ts
@@ -0,0 +1,11 @@
+export {}
+
+// Type declerations for process.env
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'production' | 'development',
+            PORT: number,
+        }
+    }
+}
