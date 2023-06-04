5527ddb006a00dfebe87ecbca4688bac792dcd02
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-04 15:40:40 +0530

    type declarations

--- src/core/utils/response.ts
+++ src/core/utils/response.ts
@@ -1,18 +1,21 @@
 import { Response } from 'express'
 
+type TData = 'Success' | 'Forbidden' | object | Array<object>
+
 interface Res {
     status: boolean,
-    data: string | object | Array<object>
+    data: TData
 }
 
 export const newPromise = async (): Promise<[string]> => {
     return Promise.resolve(['Name'])
 }
 
-export const ExpressResponse = (res: Response, status: boolean, code: number, data: string | object | Array<object> ): void => {
+export const ExpressResponse = (res: Response, status: boolean, code: number, data: TData ): void => {
     const response: Res = {
         status: status,
         data: data
     }
     res.status(code).json(response)
+    return
 }
